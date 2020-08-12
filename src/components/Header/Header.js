f74dc3f78d3e51193f01a51f6fc53b65369ab8d2
mizhidan <dj9824@126.com>
2020-08-12 18:38:48 +0800

    [danjie] feat: cart icon with count number

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,20 @@ import cart from '../../assets/cart.svg';
 import './Header.scss';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
     return (
       <div className="header">
         <h1>Store</h1>
-        <img src={cart} alt="cart-icon" className="cart-icon" />
+        <div className="cart">
+          <img src={cart} alt="cart-icon" className="cart-icon" />
+          <span className="count">{this.state.count}</span>
+        </div>
       </div>
     );
   }
